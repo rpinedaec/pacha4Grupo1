335c614a7166f5f5336cb79e6b281d40fe5b4897
Bruce Kevin Porras Miliano <bk_9_93@hotmail.com>
2020-09-16 03:47:53 -0500

    commi 5

--- ecommprj/urls.py
+++ ecommprj/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from ecommapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'cupones', views.CuponViewSet)
@@ -17,3 +19,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
