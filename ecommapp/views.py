6d957b138c8feaaab99f7dd25c39dcdb460be595
Denisse Garcia <garciasayand@gmail.com>
2020-09-17 22:00:41 -0500

    permisos

--- ecommapp/views.py
+++ ecommapp/views.py
@@ -7,34 +7,27 @@ from .serializers import CuponSerializer, Estado_pedidoSerializer, CategoriaSeri
 class CuponViewSet(viewsets.ModelViewSet):
     queryset = Cupon.objects.all()
     serializer_class = CuponSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class Estado_pedidoViewSet(viewsets.ModelViewSet):
     queryset = Estado_pedido.objects.all()
     serializer_class = Estado_pedidoSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 class Detalle_pedidoViewSet(viewsets.ModelViewSet):
     queryset = Detalle_pedido.objects.all()
     serializer_class = Detalle_pedidoSerializer
-    permission_classes = [permissions.IsAuthenticated]
