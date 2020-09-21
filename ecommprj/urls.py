66ff9d6823bd31d4018b5b3132a961ddf9d9c1b0
Denisse Garcia <garciasayand@gmail.com>
2020-09-21 12:58:46 -0500

    make it better

--- ecommprj/urls.py
+++ ecommprj/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
-    #url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
     #url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
