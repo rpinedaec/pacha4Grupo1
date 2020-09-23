ca5070fa24d1972e6bbf3506d3508ec72aa73c4b
Denisse Garcia <garciasayand@gmail.com>
2020-09-22 20:55:31 -0500

    make it better

--- ecommapp/views.py
+++ ecommapp/views.py
@@ -5,31 +5,31 @@ from .serializers import CuponSerializer, Estado_pedidoSerializer, CategoriaSeri
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class CuponViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Cupon.objects.all()
     serializer_class = CuponSerializer
 
 class Estado_pedidoViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Estado_pedido.objects.all()
     serializer_class = Estado_pedidoSerializer
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
 class ClienteViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
 
@@ -40,13 +40,13 @@ class ProductoViewSet(viewsets.ModelViewSet):
 
 class PedidoViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
 
 
 class Detalle_pedidoViewSet(viewsets.ModelViewSet):
     #permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication, SessionAuthentication)
+    #authentication_classes = (TokenAuthentication, SessionAuthentication)
     queryset = Detalle_pedido.objects.all()
     serializer_class = Detalle_pedidoSerializer
